552b923e55620ae703dd84bf80cac5fab4df8daf
Sumanth3420 <indralasumanth3420@gmail.com>
2024-06-17 15:45:52 +0530

    updated

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const produtsServiceLocalHost = 'http://localhost:8132/api'
 const usersServiceLocalHost = 'http://localhost:8232/api'
 
-const produtsServiceDevHost = 'http://34.125.84.228:5132/api'
-const usersServiceDevHost = 'http://34.125.84.228:5232/api'
+const produtsServiceDevHost = 'http://34.125.140.221:5132/api'
+const usersServiceDevHost = 'http://34.125.140.221:5232/api'
 
 const produtsServiceTestHost = ''
 const usersServiceTestHost = ''
